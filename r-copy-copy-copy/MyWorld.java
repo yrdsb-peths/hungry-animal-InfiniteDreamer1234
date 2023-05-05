dd39b7fc3e83d77be681256fe958c9d53db48de6
InfiniteDreamer1234 <130995647+InfiniteDreamer1234@users.noreply.github.com>
2023-05-05 11:11:30 -0400

    Worked on the assignment

---
+++ r-copy-copy-copy/MyWorld.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        Elephant e = new Elephant();
+        addObject(e, 300, 200);
+        
+        Bread b = new Bread();
+        addObject(b,300,0);
+    }
+}
