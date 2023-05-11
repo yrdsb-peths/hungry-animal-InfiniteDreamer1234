c1fec7069f7b9b913f808c04a50479817c044a1b
InfiniteDreamer1234 <130995647+InfiniteDreamer1234@users.noreply.github.com>
2023-05-11 08:38:51 -0400

    Update MyWorld.java

--- r-copy-copy-copy/MyWorld.java
+++ r-copy-copy-copy/MyWorld.java
@@ -13,15 +13,41 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    
+    public int score = 0;
+    Label scorelabel;
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
         
         Elephant e = new Elephant();
         addObject(e, 300, 200);
         
-        Bread b = new Bread();
-        addObject(b,300,0);
+        scorelabel = new Label(0, 80);
+        addObject(scorelabel, 50, 50);
+        
+        
+        createBread();
+    }
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    public void increaseScore()
+    {
+        score++;
+        scorelabel.setValue(score);
+    }
+    
+    public void createBread()
+    {
+        Bread bread = new Bread();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(bread, x, y);
+        
     }
 }
