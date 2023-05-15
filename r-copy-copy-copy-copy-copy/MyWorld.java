b9f0b0ea34894576f8557a58c89fb1bfa65dcf6c
InfiniteDreamer1234 <130995647+InfiniteDreamer1234@users.noreply.github.com>
2023-05-15 08:51:04 -0400

    Added author

--- r-copy-copy-copy-copy-copy/MyWorld.java
+++ r-copy-copy-copy-copy-copy/MyWorld.java
@@ -5,8 +5,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Justin Dong 
+ * @ May 2023
  */
 
 
